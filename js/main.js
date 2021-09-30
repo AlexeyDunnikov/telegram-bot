c0dbdf7ece2a8233f7177f3f38c9487daa74696a
Dunnikov Alexey <88057907+AlexeyDunnikov@users.noreply.github.com>
2021-09-30 19:41:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  jQuery(document).ready(function () {
+    jQuery(".post").addClass("hidden").viewportChecker({
+      classToAdd: "visible animated fadeIn",
+      offset: 100,
+    });
+  });
+});
